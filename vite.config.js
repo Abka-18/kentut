3414cc28a4c3caa6be79478d4efb1b9cb3b4db27
analystcmps1 <dataanalytics@telunjuk.com>
2025-04-09 17:19:47 +0700

    fix: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kentut-predictor/',
+  base: '/kentut/', // HARUS sesuai dengan nama repo GitHub kamu
 })
